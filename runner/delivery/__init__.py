65f3824df40fcf4446d2c2666fd062034528fee3
Iryna Solovyova <solovyova.iryna@gmail.com>
2026-08-06 13:35:55 -0700

    Simplify Kind of News for v0.1

--- runner/delivery/__init__.py
+++ runner/delivery/__init__.py
@@ -2,15 +2,9 @@
 
 from .base import DeliveryError, DeliveryResult
 from .buttondown import ButtondownDelivery
-from .gmail import GmailDelivery
-from .telegram import TelegramDelivery
-from .webhook import WebhookDelivery
 
 __all__ = [
     "DeliveryError",
     "DeliveryResult",
     "ButtondownDelivery",
-    "GmailDelivery",
-    "TelegramDelivery",
-    "WebhookDelivery",
 ]
